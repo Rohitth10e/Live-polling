d4f6e2df1866be38975fe86343cfaf6e22141d1a
rohith10e <rohith10e@outlook.com>
2025-09-25 02:31:54 +0530

    fix: ui/ux, toast-notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoleSelection from "./pages/RoleSelection";
 import StudentPage from "./pages/StudentPage";
 import { TeacherDashboard } from "./pages/TeacherDashboard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RoleSelection />} />
-        <Route path="/student" element={<StudentPage/>} />
-        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RoleSelection />} />
+          <Route path="/student" element={<StudentPage />} />
+          <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer
+        toastClassName="custom-toast"
+        bodyClassName="custom-toast-body"
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        theme="light"
+      />
+    </>
   );
 }
 
