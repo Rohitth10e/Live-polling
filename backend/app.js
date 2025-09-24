93d5c3dbb016eb66176a9a31cdbb16ad6bf10641
rohith10e <rohith10e@outlook.com>
2025-09-24 19:40:28 +0530

    feat: TeacherDashboard

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
 
 app.use("/api/polls", pollRoutes);
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"]
     }
 });
